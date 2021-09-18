007afbb6a0ba1297b90599b0b786623a669cee99
Michael Wei <dev.wei@gmail.com>
2021-09-17 22:46:16 -0400

    check in

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
     tonic_build::configure()
+        .out_dir("src/routeguide")
+        .type_attribute("routeguide.Point", "#[derive(Hash)]")
+        .compile(&["proto/routeguide/route_guide.proto"], &["proto"])
+        .unwrap();
+
+    tonic_build::configure()
         .out_dir("src/helloworld")
         .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
         .unwrap();
